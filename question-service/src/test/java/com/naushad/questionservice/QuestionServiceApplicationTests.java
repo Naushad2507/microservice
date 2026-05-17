f0f7500eeea9f09cf54a0b2f71be3c9c85f680ab
Naushad Akhtar <121228774+Naushad2507@users.noreply.github.com>
2026-05-17 13:25:15 +0530

    question-service First Commit

---
+++ question-service/src/test/java/com/naushad/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naushad.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

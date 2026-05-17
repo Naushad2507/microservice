1802881e5d09c8dcc04a767323fdd6d321ada478
Naushad Akhtar <121228774+Naushad2507@users.noreply.github.com>
2026-05-17 13:26:40 +0530

    service-registry First Commit

---
+++ service-registry/src/test/java/com/naushad/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naushad.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
